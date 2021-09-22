3a91bcc82ed0368de012fb6e0dd13e633875706f
thiviyam <89219865+thiviyam@users.noreply.github.com>
2021-09-23 00:31:20 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,4 @@
+insert into Stud_Login values ('thiviya','Doodle1718')
+insert into Stud_Login values ('ravi','animifreak')
+insert into Stud_Login values ('murugu','kadamba')
+insert into Stud_Login values ('raji','omnamasiva')
